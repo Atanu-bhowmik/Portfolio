c85352015b46ce335595f2d184dc34fc03d23eb9
atanu-bhowmik-21 <62740377+atanu-bhowmik-21@users.noreply.github.com>
2024-01-22 21:50:40 +0530

    added a new home page

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
+import "./styles.scss";
 const Home = () => {
-    return (
-        <div> Home Page</div>
-    )
-}
+  const navigate = useNavigate();
+  const toContactpage = () => {
+    navigate("/contact");
+  };
+  return (
+    <section id="home" className="home">
+      <div className="home__text-wrapper">
+        <h1>
+          Hello, I'm Atanu
+          <br />A Developer
+        </h1>
+      </div>
+
+      <div className="home__contact-me">
+        <button onClick={toContactpage}>Lets Talk</button>
+      </div>
+    </section>
+  );
+};
 
 export default Home;
